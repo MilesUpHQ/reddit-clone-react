61589f554e6a8ca803adae1fa2175ab93bdb094a
tushar R <tusharrravindran@gmail.com>
2023-01-23 21:20:44 +0530

    community_form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home/Home';
 import Signin from './components/Form/Signin';
 import Signup from './components/Form/Signup';
 import Navbar from './components/Navbar/Navbar';
+import Community from './components/Form/Community';
 import { BrowserRouter as Router,	Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
+          <Route path="/Community" element={<Community />} />
         </Routes>
       </Router>
     </div>
