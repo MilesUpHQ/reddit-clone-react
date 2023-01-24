2c997f5416f5511bf799d28150041bf567f43433
mithun30052001 <lmithun01@gmail.com>
2023-01-24 13:33:08 +0530

    both create and post type tabs added to homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Create_Post from './Create_Post';
+import Tab_List from './Tab_List';
+
 
 const Home = () => {
   return (
     <div>
       <h1>Reddit Clone</h1>
+     <div class="community_post">
+      <div class="row">
+        <div class="col-sm-8">
+          < Create_Post />
+          < Tab_List />
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
