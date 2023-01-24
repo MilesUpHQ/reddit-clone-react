a0fbf969a98e7fb9041811f17ed69dd87bf48412
tushar R <tusharrravindran@gmail.com>
2023-01-24 10:04:48 +0530

    resolves App.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Form from './components/Form/Form';
 import Navbar from './components/Navbar/Navbar';
 import JsonData from './data/data.json'
 import NewPost from './components/Post/NewPost';
+import Community from './components/Form/Community';
+import Profile from './components/Accounts/Profile';
+import Setting from './components/Accounts/Settings';
+import Signout from './components/Accounts/Signout';
 import { BrowserRouter as Router,	Routes, Route } from 'react-router-dom';
 
 
@@ -17,40 +21,10 @@ function App() {
           <Route path="/signup" element={<Form data = {JsonData.form.signup} />} />
           <Route path="/signin" element={<Form data = {JsonData.form.signin} />} />
           <Route path="/new" element={<NewPost />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-}
-
-export default App;
-import './App.css';
-import Home from './components/Home/Home';
-import Signin from './components/Form/Signin';
-import Signup from './components/Form/Signup';
-import Navbar from './components/Navbar/Navbar';
-import Community from './components/Form/Community';
-import Profile from './components/Accounts/Profile';
-import Setting from './components/Accounts/Settings';
-import Signout from './components/Accounts/Signout';
-
-
-import { BrowserRouter as Router,	Routes, Route } from 'react-router-dom';
-
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Router>
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
           <Route path="/Community" element={<Community />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Settings" element={<Setting />} />
           <Route path="/Signout" element={<Signout />} />
-
         </Routes>
       </Router>
     </div>
