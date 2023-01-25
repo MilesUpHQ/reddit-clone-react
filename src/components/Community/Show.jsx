242b90d5310c6d32afb23bb5a36143f2cfab0ba9
Vasanthakumar <vasanthakumara117@gmail.com>
2023-01-25 12:43:15 +0530

    community show page functionallity added which displays the community

--- src/components/Community/Show.jsx
+++ src/components/Community/Show.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import Index from './Index'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
+const Community_URL = 'http://localhost:3000/api/v1/communities/'
+
+function get_community_data(community_id) {
+  return axios.get(Community_URL + community_id).then((response) => response.data)
+}
 
 const Show = () => {
+  const [community, setCommunity] = useState([]);
+  const navigate = useNavigate()
+  let { id } = useParams();
+
+  useEffect(() => {
+    let mounted = true;
+    get_community_data(id).then((items) => {
+      if (mounted) {
+        setCommunity(items);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
   return (
     <div>
-      <Index />
+      <div className="card border-light" key={community.id}>
+        <div className="card-body border">
+          <h4 className="card-title">Community Name : {community.name}</h4>
+          <p className="card-text">URL : {community.url}</p>
+          <p className="card-text">Rules : {community.rules}</p>
+          <Button className='m-2'>Edit</Button>
+          <Button className='m-2'>Delete</Button>
+        </div>
+      </div>
     </div>
   )
 }
