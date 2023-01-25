02ade865e254481080f78772765b6d6c73673b98
tushar R <tusharrravindran@gmail.com>
2023-01-25 14:26:31 +0530

    post api for form completed

--- src/components/Post/CommunityTitle.jsx
+++ src/components/Post/CommunityTitle.jsx
@@ -1,29 +1,31 @@
+import { post } from 'jquery'
 import React from 'react'
 import '../../css/post.css'
 
-const CommunityTitle = () => {
-    return (
-        <div>
-          <form>
-            <div className = "row mt-3">
-              <div className = "col-sm-12">
-                <div className = "card rounded mb-3">
-                  <div className = "form-group">
-                    <input type='text' id = "community_id" className = "form-select search-input-navbar community_select" placeholder='Choose a community' />
-                  </div>
-                </div>
+
+const CommunityTitle = ({ onChange }) => {
+  return (
+    <div>
+      <form>
+        <div className="row mt-3">
+          <div className="col-sm-12">
+            <div className="card rounded mb-3">
+              <div className="form-group">
+                <input type='text' id="community_id" className="form-select search-input-navbar community_select" placeholder='Choose a community' onChange={onChange} />
               </div>
             </div>
-            <div className = "rounded mb-3">
-              <div className = "create-post m-3">
-                <div className = "form-group">
-                  <input type = "text" id = "title" className = "form-control" placeholder = 'Title' />
-                </div>
-              </div>
-            </div>
-           </form>
+          </div>
         </div>
-    )
+        <div className="rounded mb-3">
+          <div className="create-post m-3">
+            <div className="form-group">
+              <input type="text" id="title" className="form-control" placeholder='Title' name='title' onChange={onChange} value={post.title} />
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default CommunityTitle
