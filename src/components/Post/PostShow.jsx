3cba064ec3b7d01a7fa3d4944020910ec7c7404f
Aboorva1 <abookannan567@gmail.com>
2023-01-27 13:36:46 +0530

    Post show page created

--- src/components/Post/PostShow.jsx
+++ src/components/Post/PostShow.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-import { confirmAlert } from 'react-confirm-alert';
+import { useParams, Link } from 'react-router-dom';
+import Reactions from './Reactions';
+import { FaArrowUp, FaArrowDown} from 'react-icons/fa';
+import '../../css/post.css'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 
 const Post_URL = 'http://localhost:3000/api/v1/posts/'
 
@@ -27,10 +31,48 @@ const PostShow = () => {
 
   return (
     <div>
-      <div className="card border-light" key={post.id}>
-        <div className="card-body border">
-          <h4 className="card-title">Post title : {post.title}</h4>
-          <p className="card-text">URL : {post.body}</p>
+      <div className="show_post">
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-sm-8">
+              <div className="card mb-3 ">
+                <div className = "mt-1">
+                  <div className="col-10 m-0" >
+                    <div><FaArrowUp /></div>
+                    <div><FaArrowDown /></div>
+                    <div className = "post-head">
+                      <div className = "col-5 ms-5">
+                        <p>
+                          <strong><Link to={`/r/${post.id}`} className="text-dark">r/{post.id}</Link></strong>
+                          <small> Posted by{' '}<Link to={`/u/${post.id}`}>u/{post.id}</Link>{' '}</small>
+                        </p>
+                      </div> 
+                    </div>
+                  </div>
+                </div>
+                <div className="" key={post.id}>
+                  <div className='mt-0 ms-5'>
+                    <h4>Post title : {post.title}</h4>
+                    <p>{post.body}</p>
+                    <Reactions Post_URL ={Post_URL} get_post_data={get_post_data}/>
+                  </div>
+                  <form action="">
+                    <div className="create-post m-5">
+                      <div className="form-group mb-3">
+                        <h6>Comment as</h6>
+                        <ReactQuill placeholder="Your Comment goes here" value = {""} style={{ height: '200px' }} />
+                      </div>
+                    </div>
+                    <div className="m-3">
+                      <input type="submit" value="Submit comment" />
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4">
+            </div>
+          </div>
         </div>
       </div>
     </div>
