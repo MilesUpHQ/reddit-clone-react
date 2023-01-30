174ff54ce0f449e5abdfd4446dbf3b0c91d9de82
mithun30052001 <lmithun01@gmail.com>
2023-01-30 15:45:40 +0530

    Discussion form refactored

--- src/components/Post/DiscussionForm.jsx
+++ src/components/Post/DiscussionForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -7,21 +7,26 @@ import CommunityTitle from './CommunityTitle'
 import '../../css/post.css'
 import '../../css/warning.css'
 import axios from 'axios';
+import { post } from 'jquery';
 import { toast } from 'react-toastify';
-
-const Post_URL = 'http://localhost:3000/api/v1/posts/'
-
+const Post_URL = `http://localhost:3000/api/v1/communities/${post.community_id}/posts/`;
 const DiscussionForm = () => {
-
+  const [communities, setCommunities] = useState([])
+  const Community_URL = 'http://localhost:3000/api/v1/communities/'
+  useEffect(() => {
+    fetch(Community_URL)
+      .then(response => response.json())
+      .then(data => setCommunities(data))
+  }, [])
+  const account = JSON.parse(localStorage.getItem('account'))
   const navigate = useNavigate();
   // const [errors, setErrors] = useState('');
   const [post, setPost] = useState({
-    account_id: 1,
-    community_id: 1,
+    account_id: account.id,
+    community_id: '' || 1,
     title: '',
     body: ''
   });
-
   const set_new_post = async (post) => {
     await axios.post(Post_URL, { post }).then((response) => {
       if (response.status === 201) {
@@ -33,17 +38,13 @@ const DiscussionForm = () => {
       toast.error("An error occured while submitting the Post");
     })
   }
-
   const onChange = (event) => {
     setPost({ ...post, [event.target.name]: event.target.value });
+    console.log(event.target.value)
   }
-
-
   const handleChange = (content, delta, source, editor) => {
     setPost({ ...post, body: editor.getText().trim() });
   }
-
-
   const onSubmit = (event) => {
     event.preventDefault();
     if (!post.body) {
@@ -52,11 +53,22 @@ const DiscussionForm = () => {
     }
     set_new_post(post);
   }
-
-
   return (
     <div>
       <form action="">
+        <div className="row mt-3">
+          <div className="col-sm-12">
+            <div className="card rounded mb-3">
+              <div className="form-group">
+                <select id="community_id" className="form-select search-input-navbar community_select" placeholder='Choose a community' name="community_id" value={post.community_id} onChange={onChange}>
+                  {communities.map(community => (
+                    <option key={community.id} value={community.id}>{community.name}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
         <CommunityTitle onChange={onChange} />
         <div className="create-post m-3">
           <div className="form-group mb-3">
@@ -67,7 +79,13 @@ const DiscussionForm = () => {
         <div>
           <div className="float-right">
             <div className="join-btn  create-post-btn mb-4">
-              <input type="submit" value="Save as draft" className="text-white" />
+             
+              <input type="submit" value="Save as draft" className="text-white" 
+             onClick={() => {
+              set_new_post({...post, is_drafted: true});
+              }
+            }
+          />
             </div>
             <div className="join-btn create-post-btn mb-4">
               <input type="submit" value="Publish" className="text-white" onClick={onSubmit} />
@@ -79,3 +97,17 @@ const DiscussionForm = () => {
   )
 }
 export default DiscussionForm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
