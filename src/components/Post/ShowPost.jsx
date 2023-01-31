b2e06894268966ad5f18cfc7a5bf93f928a708bf
tushar R <57782379+tusharravindran@users.noreply.github.com>
2023-01-31 11:47:08 +0530

    Revert "Feat/post show"

--- src/components/Post/ShowPost.jsx
+++ src/components/Post/ShowPost.jsx
@@ -7,18 +7,7 @@ function get_community_posts(community_id) {
   return axios.get(Community_URL + community_id + '/posts').then((response) => response.data)
 }
 
-const PostLists = ({community_id}) => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    let mounted = true;
-    get_community_posts(community_id).then((items) => {
-      if (mounted) {
-        setPosts(items);
-      }
-    });
-    return () => (mounted = false);
-  }, []);
+const ShowPost = ({ posts, community_id }) => {
 
   return (
     <div>
@@ -35,4 +24,4 @@ const PostLists = ({community_id}) => {
   )
 }
 
-export default PostLists;
+export default ShowPost;
