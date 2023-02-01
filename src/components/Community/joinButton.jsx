9976520ca220a360a3a6a6da4dad956d7bfaa557
Vasanthakumar <vasanthakumara117@gmail.com>
2023-02-01 11:26:29 +0530

    added axios method to suscribe

--- src/components/Community/joinButton.jsx
+++ src/components/Community/joinButton.jsx
@@ -1,15 +1,34 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
-function JoinButton() {
+function JoinButton({ isSubribed , setIsSubscribed}) {
+  const { id } = useParams();
+  const account = JSON.parse(localStorage.getItem('account'))
+  const Community_Subscribe_URL = `http://localhost:3000/api/v1/communities/${id}/subscriptions`
+
   const [isClicked, setIsClicked] = useState(false);
+
+  const Subscribe = async() => {
+    return axios.post(Community_Subscribe_URL, {
+      account_id: account.id
+    }).then((response) => {
+      setIsSubscribed(true)
+      toast.success("Joined Community!")
+    }).catch((error)=>{
+      toast.error("Something went wrong!")
+      console.log(error.response.data)
+    })
+  }
 
   return (
     <button className="create-post join-btn"
       onMouseEnter={() => setIsClicked(false)}
       onMouseLeave={() => setIsClicked(false)}
-      onClick={() => setIsClicked(!isClicked)}
+      onClick={Subscribe}
     >
-      {isClicked ? "Leave" : "Join"}
+      {isSubribed ? "Leave" : "Join"}
     </button>
   );
 }
