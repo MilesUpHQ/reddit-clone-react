0fcfbaae365d1187dc029d9e80ad9754ad68287b
tushar R <tusharrravindran@gmail.com>
2023-02-03 12:09:16 +0530

    Completed Reports

--- src/components/Post/Reactions.jsx
+++ src/components/Post/Reactions.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import { confirmAlert } from 'react-confirm-alert';
 import { FaEdit, FaTrash, FaTimes, FaComment, FaFlag } from 'react-icons/fa';
 import EditPost from '../../components/Post/Form/EditPost';
-import ReportForm from './ReportForm'
 import { toast } from 'react-toastify';
 
 function delete_post(Post_URL) {
@@ -12,12 +11,39 @@ function delete_post(Post_URL) {
 }
 
 const Reactions = () => {
+  const categories = [
+    {
+      id: 1,
+      name: "Offensive Content",
+      report_reasons: [
+        { id: 1, reason: "Hate speech" },
+        { id: 2, reason: "Nudity or Sexual Content" },
+        { id: 3, reason: "Violence or Threats" }
+      ]
+    },
+    {
+      id: 2,
+      name: "Spam or Misleading",
+      report_reasons: [
+        { id: 1, reason: "False information" },
+        { id: 2, reason: "Scam or phishing" },
+        { id: 3, reason: "Spam" }
+      ]
+    }
+  ];
   const [post, setPost] = useState([]);
+
+  const [selectedCategoryId, setSelectedCategoryId] = useState(null);
+  const [selectedReasonId, setSelectedReasonId] = useState(null);
+  const [selectedReasonName, setSelectedReasonName] = useState(null);
+  const [selectedCategoryName, setSelectedCategoryName] = useState(null);
+
   const navigate = useNavigate()
   let { community_id, id } = useParams();
   const Post_URL = `http://localhost:3000/api/v1/communities/${community_id}/posts/${id}/`;
 
   const [isOpen, setIsOpen] = useState(false);
+  const account = JSON.parse(localStorage.getItem('account'))
 
   const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
@@ -81,8 +107,27 @@ const Reactions = () => {
   }
 
   const ReportHandler = () => {
-    console.log("report")
+    const reportDetails = {
+      report_category_id: selectedCategoryId,
+      report_reason_id: selectedReasonId,
+      report_reason_name: selectedReasonName,
+      report_categories_name: selectedCategoryName,
+      account_id: account.id,
+      post_id: id
+    };
+    axios.post(Post_URL + 'reports', { report: reportDetails }).then((response) => {
+      if (response.status === 201) {
+        toast.success("Report submitted successfully!");
+        navigate(`/r/${community_id}/p` + id)
+      }
+    })
+      .catch(error => {
+        console.log(error.response.data);
+        toast.error("An error occured while submitting the report");
+      });
+    console.log("reported")
   }
+
   return (
     <div className='col-10'>
       <div className="border-light" key={post.id}>
@@ -103,10 +148,47 @@ const Reactions = () => {
                 </div>
                 <div className="modal-body">
                   <p>Following are the Reasons for Reporting</p>
-                  <ReportForm />
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-danger" onClick={ReportHandler}>Report</button>
+                  <form>
+                    {categories.map(category => (
+                      <div key={category.id} className="form-group">
+                        <div className="form-check">
+                          <input
+                            type="radio"
+                            className="form-check-input"
+                            checked={selectedCategoryId === category.id}
+                            onChange={() => {
+                              setSelectedCategoryId(category.id);
+                              setSelectedCategoryName(category.name);
+                            }}
+                          />
+                          <label className="form-check-label">{category.name}</label>
+                          <br />
+                        </div>
+                        {selectedCategoryId === category.id && (
+                          <div className="sub-comment">
+                            {category.report_reasons.map(reason => (
+                              <div key={reason.id} className="form-check">
+                                <input
+                                  type="radio"
+                                  className="form-check-input"
+                                  checked={selectedReasonId === reason.id}
+                                  onChange={() => {
+                                    setSelectedReasonId(reason.id);
+                                    setSelectedReasonName(reason.reason);
+                                  }}
+                                />
+                                <label className="form-check-label">{reason.reason}</label>
+                                <br />
+                              </div>
+                            ))}
+                          </div>
+                        )}
+                      </div>
+                    ))}
+                    <div className="modal-footer">
+                      <button type="submit" className="btn btn-danger" onClick={ReportHandler}>Report</button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
