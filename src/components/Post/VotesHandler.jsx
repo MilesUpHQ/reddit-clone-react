7daf488f8760f63ab932edf92ae16ee1c8048c42
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-02-05 08:57:38 +0530

    Upvote functionality is handled for create

--- src/components/Post/VotesHandler.jsx
+++ src/components/Post/VotesHandler.jsx
@@ -1,13 +1,26 @@
-import {React, useState } from 'react'
+import { React, useState } from 'react'
 import { TbArrowBigTop, TbArrowBigDown } from 'react-icons/tb'
 
 
 const VotesHandler = ({ communityId, postId, voteCount }) => {
+    const account = JSON.parse(localStorage.getItem('account'))
     const [count, setCount] = useState(voteCount);
-       return (
+    const handleUpvote = async () => {
+        axios
+            .post(`http://localhost:3000/api/v1/communities/${communityId}/posts/${postId}/votes`, {
+                vote: {
+                    value: 1,
+                    account_id: account.id
+                },
+            })
+            .then((response) => {
+                setCount(response.data);
+            });
+    }
+    return (
         <div>
             <div className={`upvote`}>
-                <TbArrowBigTop />
+                <TbArrowBigTop onClick={handleUpvote} />
             </div>
             <span className="font-weight-bold score">{count}</span>
             <div className={`downvote`}>
