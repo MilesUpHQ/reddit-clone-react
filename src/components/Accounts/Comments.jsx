a10d1847e4b37fea3ecde05d6645602e26ca3913
mithun30052001 <lmithun01@gmail.com>
2023-02-05 09:42:56 +0530

    Design issue in the profile comments page fixed

--- src/components/Accounts/Comments.jsx
+++ src/components/Accounts/Comments.jsx
@@ -25,18 +25,16 @@ const Comments = () => {
     <>
       {comments.length ? (
         comments.map(comment => (
-          <div style={{ height: '165px' }} className="card post-card mb-3 shadow">
+          <div style={{ height: '70px' }} className="card post-card mb-3 shadow">
             <div className="row m-0 bg-white">
               <div className="ml-3">
                 <img src={reddit_logo} alt="" className="post-list-profile-img" />
-                <div className="d-flex gap-1 post-list-head mt-2">
-                  <a href={`/profile`}><b> u/{comment.username}</b></a>
-                  commented on  <a href={`/r/1/p/${comment.post_id}`}>
-                    {comment.post_title} </a><b>{moment(comment.created_at).fromNow()}</b>
-                </div>
+                  <a href={`/profile`}><b> u/{comment.username+" "}</b></a>
+                    commented on  <a href={`/r/1/p/${comment.post_id}`}>
+                    {comment.post_title+ " "} </a><b>{moment(comment.created_at).fromNow()}</b>
               </div>
               <b>
-                <p className="m-0">Comment : <a style={{ textDecoration: 'none' }} href={`/r/1/p/${comment.post_id}?highlight=${comment.message}`}>
+                <p className="m-1">Comment : <a style={{ textDecoration: 'none' }} href={`/r/1/p/${comment.post_id}?highlight=${comment.message}`}>
                   <Markup content={truncateString(comment.message, 50)}></Markup></a></p>
               </b>
             </div>
