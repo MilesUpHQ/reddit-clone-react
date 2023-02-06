775860cecb2bbf1e13c7f21d4a0686d8e143be11
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-02-06 11:33:49 +0530

    Error is fixed in profile page votes

--- src/components/Post/VotesHandler.jsx
+++ src/components/Post/VotesHandler.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 const VotesHandler = ({ post, communityId, postId, voteCount }) => {
     const account = JSON.parse(localStorage.getItem('account'))
-    const postvote = account ? post.votes.find(vote => vote.account_id === account.id) : "";
+    const postvote = account && post.votes ? post.votes.find(vote => vote.account_id === account.id) : "";
     const [count, setCount] = useState(voteCount);
     const [upvoteClass, setupvoteClass] = useState(postvote && postvote.value === 1 ? "voted" : "");
     const [downvoteClass, setdownvoteClass] = useState(postvote && postvote.value === -1 ? "voted" : "");
