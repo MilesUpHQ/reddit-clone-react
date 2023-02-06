c89c1ef1764a06539f5f3465b9b57888ffdfb0d7
Aboorva1 <abookannan567@gmail.com>
2023-02-06 16:14:56 +0530

    my communities list displayed in dropdown

--- src/components/Navbar/SubscriptionsList.jsx
+++ src/components/Navbar/SubscriptionsList.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import reddit_logo from '../../images/reddit-logo.png';
+import '../../css/post.css';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const SubscriptionsList = ({ accountId }) => {
   const [subscriptions, setSubscriptions] = useState([]);
@@ -17,7 +20,11 @@ const SubscriptionsList = ({ accountId }) => {
     <div>
       <ul>
         {subscriptions.map((subscription) => (
-          <div key={subscription.id}>{subscription.community && subscription.community.name}</div>
+          <div key={subscription.id}>
+            <Dropdown.Item href={`http://localhost:3001/r/${subscription.community_id}`} className="text-dark">
+              r/{subscription.community && subscription.community.name}
+            </Dropdown.Item>
+          </div>
         ))}
       </ul>
     </div>
