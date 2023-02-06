fa29174256a5d5a9b334975ce90ba1eb80f87566
Aboorva1 <abookannan567@gmail.com>
2023-02-06 10:08:29 +0530

    On clicking community name, cover and profile image, redirected to community show

--- src/components/Post/CommunityDetails.jsx
+++ src/components/Post/CommunityDetails.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import cover_image from '../../images/Cover-Image.jpg';
 import reddit_logo from '../../images/reddit-logo.png'
 
 const CommunityDetails = ({ post }) => {
 
-  let { id } = useParams();
+  let { community_id } = useParams();
 
   useEffect(() => {
     let mounted = true;
@@ -18,9 +18,19 @@ const CommunityDetails = ({ post }) => {
       <div className="card" key={post.id}>
         <div className="card-body">
           <div className="text-center">
-            <img src={cover_image} className="card-img-top home-small-cover"></img>
-            <img src={reddit_logo} alt="" className="ml-3 home-small-profile" />
-            <h5 className="card-title">{ post.community && post.community.name}</h5>
+            <Link to={`/r/${community_id}`}>
+              {post.community && post.community.cover_image && post.community.cover_image.url ? [
+                <img src={`http://localhost:3000${post.community.cover_image.url}`} alt="" className="card-img-top home-small-cover" />
+              ] : [
+                <img src={cover_image} alt="" className="card-img-top home-small-cover" />
+              ]}
+              {post.community && post.community.profile_image && post.community.profile_image.url ? [
+                <img src={`http://localhost:3000${post.community.profile_image.url}`} alt="" className="ml-3 home-small-profile" />
+              ] : [
+                <img src={reddit_logo} alt="" className="ml-3 home-small-profile" />
+              ]}
+            </Link>
+            <h5 className="card-title"><Link to={`/r/${community_id}`}>{post.community && post.community.name}</Link></h5>
           </div>
         </div>
       </div>
