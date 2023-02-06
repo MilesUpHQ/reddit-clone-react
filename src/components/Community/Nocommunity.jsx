2cf7fd3dd9349b06695199fba6fd5264b90b5b1e
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-02-07 04:52:38 +0530

    No community image added

--- src/components/Community/Nocommunity.jsx
+++ src/components/Community/Nocommunity.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import cover_image from '../../images/Cover-Image.jpg';
+import no_community from '../../images/no_community.jpg';
 import { Link } from "react-router-dom";
 const Nocommunity = () => {
     return (
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh"}}>
+        <div style={{ backgroundColor: "lightgray", display: "flex", alignItems: "center", justifyContent: "center", height: "100vh"}}>
             <div className="text-center">
-                <img src={`${cover_image}`} height="100px" width="100px" alt="error" style={{ borderRadius: "50%" }} />
+                <img src={`${no_community}`} height="100px" width="100px" alt="error" style={{ borderRadius: "50%" }} />
                 <p className="fs-3">
                     <span className="text-danger">Sorry </span> There aren’t any communities on Reddit with that name.
                 </p>
