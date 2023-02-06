fa97c9cdf9c3b0c5be61c0aaeed05f3c6d3ce156
mithun30052001 <lmithun01@gmail.com>
2023-02-06 16:17:31 +0530

    Design issue in the comment show page fixed

--- src/components/Post/Comments.jsx
+++ src/components/Post/Comments.jsx
@@ -50,13 +50,13 @@ const Comments = ({ highlight }) => {
           ] : [
             <img src={profile_image} alt="" className="profile-img-navbar" />
           ]}
-          <strong>{comment.account.first_name}</strong>
-          <div className='ms-4'>
+          <strong>{comment.account.first_name} <span className="text-muted ms-2">{moment(comment.created_at).fromNow()}</span></strong>
+
+          <div className='ms-4 ml-4'>
             <Markup content={comment.message} />
           </div>
         </div>
         <div class="fl">
-          <p className="text-muted m-l-30">{moment(comment.created_at).fromNow()}</p>
           <a href="#" onClick={(event) => handleClick(event, comment.id)}>Reply</a>
           {selectedComment === comment.id && <Form parent={comment.id} comment_id={comment.id} />}
           <hr />
