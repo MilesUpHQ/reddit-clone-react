384e69b86ba4f73e6b637e43d77124a9641dce8a
tushar R <tusharrravindran@gmail.com>
2023-02-07 16:54:45 +0530

    POST BODY UI

--- src/components/Post/PostShow.jsx
+++ src/components/Post/PostShow.jsx
@@ -35,65 +35,67 @@ const PostShow = () => {
     });
     return () => (mounted = false);
   }, []);
-  return ( 
+  return (
     <>
-    { post.title ? (
-    <div>
-      <div className="show_post">
-        <div className="container mt-5">
-          <div className="row">
-            <div className="col-sm-8">
-              <div className="card mb-3 ">
-                <div className="show-content mt-1">
-                  <div className="col-0 ms-3 mt-2" >
-                  <VotesHandler 
-                    post={post}
-                    communityId={post.community_id}
-                    postId={post.id}
-                    voteCount={post.vote_count}
-                  />
-                  </div>
-                  <div className="post-head ms-3">
-                    <div className="row">
-                      <p key={post.id}>
-                        {post.community && post.community.profile_image && post.community.profile_image.url ? [
-                          <img src={`http://localhost:3000${post.community.profile_image.url}`} alt="" className="post-list-profile-img mr-1" />
-                        ] : [
-                          <img src={reddit_logo} alt="" className="post-list-profile-img mr-1" />
-                        ]}
-                        <strong><Link to={`/r/${community_id}`} className="text-dark">r/{post.community && post.community.name}</Link></strong>
-                        <small> Posted by{' '}<Link to='/'> u/{post.account && post.account.username} </Link>{moment(post.created_at).fromNow()}</small>
-                      </p>
+      {post.title ? (
+        <div>
+          <div className="show_post">
+            <div className="container mt-5">
+              <div className="row">
+                <div className="col-sm-8">
+                  <div className="card mb-3 ">
+                    <div className="show-content mt-1">
+                      <div className="col-0 ms-3 mt-2" >
+                        <VotesHandler
+                          post={post}
+                          communityId={post.community_id}
+                          postId={post.id}
+                          voteCount={post.vote_count}
+                        />
+                      </div>
+                      <div className="post-head ms-3">
+                        <div className="row">
+                          <p key={post.id}>
+                            {post.community && post.community.profile_image && post.community.profile_image.url ? [
+                              <img src={`http://localhost:3000${post.community.profile_image.url}`} alt="" className="post-list-profile-img mr-1" />
+                            ] : [
+                              <img src={reddit_logo} alt="" className="post-list-profile-img mr-1" />
+                            ]}
+                            <strong><Link to={`/r/${community_id}`} className="text-dark">r/{post.community && post.community.name}</Link></strong>
+                            <small> Posted by{' '}<Link to='/'> u/{post.account && post.account.username} </Link>{moment(post.created_at).fromNow()}</small>
+                          </p>
+                        </div>
+                        <strong><h2 className="ms-2">{post.title}</h2> </strong>
+                      </div>
                     </div>
-                    <strong><h2 className="ms-2">{post.title}</h2> </strong>
+                    <div className="" key={post.id}>
+                      <div className="post-body">
+                        <Markup content={post.body}></Markup>
+                        <div className='mt-4 ms-5'>
+                        {post.isclosed ? null:<Reactions Post_URL={Post_URL} get_post_data={get_post_data} />}
+                        </div>
+                      </div>
+                      {post.isclosed ? <aside className="card-body" style={{ fontSize: '20px', color: 'red',textAlign: 'center' }}>
+                        Post Closed By Moderator due to Various reasons. For further Details Contact Admin</aside>
+                        : <Form parent={null} comment_id={null} />}
+                    </div>
+                    <div className="commentssection">
+                      <Comments post={post} parent={null} highlight={highlight} />
+                    </div>
                   </div>
                 </div>
-                <div className="" key={post.id}>
-                  <div className='mt-4 ms-5'>
-                    <Markup content={post.body}></Markup>
-                    {post.isclosed ? null : <Reactions Post_URL={Post_URL} get_post_data={get_post_data} />}
-                  </div>
-                  {post.isclosed ? <p className="card-body" style={{ fontSize: '20px', color: 'red' }}>
-                    Post Closed By Admin.For further Details Contact Admin</p>
-                    : <Form parent={null} comment_id={null} />}
-                </div>
-                <div className="commentssection">
-                  <Comments post={post} parent={null} highlight={highlight} />
+                <div className="col-sm-4">
+                  <CommunityDetails post={post} />
                 </div>
               </div>
             </div>
-            <div className="col-sm-4">
-              <CommunityDetails post={post} />
-            </div>
           </div>
         </div>
-      </div>
-    </div>
-    ):(
-      <Nopost />
-    )
-  }
-  </>
+      ) : (
+        <Nopost />
+      )
+      }
+    </>
   )
 }
 export default PostShow
