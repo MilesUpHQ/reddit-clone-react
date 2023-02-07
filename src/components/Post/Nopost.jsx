52ccba5f68a3e653cbc97c31bcfb79f46ab329a2
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-02-07 06:09:42 +0530

    No post page is redesigned

--- src/components/Post/Nopost.jsx
+++ src/components/Post/Nopost.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import no_post from '../../images/no_post.png';
+import { Link } from "react-router-dom";
 
 const Nopost = () => {
-  return (
-    <div>
-      <h1>Sorry Something went wrong</h1>
-    </div>
-  )
-}
+    return (
+        <div style={{ backgroundColor: "lightgray", display: "flex", alignItems: "center", justifyContent: "center", height: "90vh" }}>
+            <div className="text-center">
+                <img src={`${no_post}`} height="300px" alt="error" style={{ borderRadius: "50%" }} />
+                <p className="fs-3">
+                    Somwthing went wrong
+                </p>
+                <Link to="/" className="join-btn">
+                    Go Home
+                </Link>
+            </div>
+        </div>
+    );
+};
 
-export default Nopost
+export default Nopost;
