e438d8abe5fec3758b80ec963f754b0decbea252
Aboorva1 <abookannan567@gmail.com>
2023-02-07 13:30:54 +0530

    Redirected to signin page if user not logged in

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -41,60 +41,60 @@ const navbar = () => {
               <Dropdown.Item href="/r">All Communities</Dropdown.Item>
               <Dropdown.Item href="/r/">Top Trending</Dropdown.Item>
               <div className="mt-2 ms-2 mb-2 text-muted"> My Communities</div>
-              <Dropdown.Item href="/r/new">Create a Community</Dropdown.Item>
-              { account && <SubscriptionsList accountId = {account.id} />}
-            </Dropdown.Menu>
-          </Dropdown>
+              <Dropdown.Item href={account ? "/r/new/" : "/signin"}>Create Community</Dropdown.Item>
+            {account && <SubscriptionsList accountId={account.id} />}
+          </Dropdown.Menu>
+        </Dropdown>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <form action="get">
+                <div className="">
+                  < Autocomplete />
+                </div>
+              </form>
+            </li>
+          </ul>
+        </div>
+
+        {account ? [
+          <div className="d-flex gap-2">
+            <div className="karma">
+              2
+              <FaStar className='  text-warning' />
+            </div>
+            <Dropdown>
+              <Dropdown.Toggle variant="transparent nav-link text-muted" id="dropdown-basic">
+                {account && account.profile_image && account.profile_image.url ? [
+                  <img src={`${account_url}${account.profile_image.url}`} alt="" className="profile-img-navbar" />
+                ] : [
+                  <img src={profile_image} alt="" className="profile-img-navbar" />
+                ]}
+                {account.username}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+                <Dropdown.Item href="/settings">Settings</Dropdown.Item>
+                <Dropdown.Item onClick={Signout}>Sign Out</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        ] : [
+          <div className="">
             <ul className="navbar-nav">
-              <li className="nav-item dropdown">
-                <form action="get">
-                  <div className="">
-                    < Autocomplete />
-                  </div>
-                </form>
+              <li className="nav-item">
+                <a href="/signup" className="nav-link">Sign Up</a>
+              </li>
+              <li className="nav-item">
+                <a href="/signin" className="nav-link">Log In</a>
               </li>
             </ul>
           </div>
+        ]}
 
-          {account ? [
-            <div className="d-flex gap-2">
-              <div className="karma">
-                2
-                <FaStar className='  text-warning' />
-              </div>
-              <Dropdown>
-                <Dropdown.Toggle variant="transparent nav-link text-muted" id="dropdown-basic">
-                  {account && account.profile_image && account.profile_image.url ? [
-                    <img src={`${account_url}${account.profile_image.url}`} alt="" className="profile-img-navbar" />
-                  ] : [
-                    <img src={profile_image} alt="" className="profile-img-navbar" />
-                  ]}
-                  {account.username}
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="/profile">Profile</Dropdown.Item>
-                  <Dropdown.Item href="/settings">Settings</Dropdown.Item>
-                  <Dropdown.Item onClick={Signout}>Sign Out</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </div>
-          ] : [
-            <div className="">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a href="/signup" className="nav-link">Sign Up</a>
-                </li>
-                <li className="nav-item">
-                  <a href="/signin" className="nav-link">Log In</a>
-                </li>
-              </ul>
-            </div>
-          ]}
-
-        </nav>
-      </div >
+      </nav>
+    </div >
     </div >
   )
 }
