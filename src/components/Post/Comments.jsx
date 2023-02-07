44652688faddcdd6aa3b4ffb70a6e96e4d7fb067
tushar R <tusharrravindran@gmail.com>
2023-02-07 13:28:36 +0530

    OC for ORGINAL CONTENT IN COMMENTS

--- src/components/Post/Comments.jsx
+++ src/components/Post/Comments.jsx
@@ -7,7 +7,6 @@ import moment from 'moment';
 import Form from '../Comment/Form';
 import PostApi from '../Home/PostApi';
 import { Markup } from 'interweave';
-import { post } from 'jquery';
 
 
 const Comments = ({ highlight }) => {
@@ -51,13 +50,13 @@ const Comments = ({ highlight }) => {
           ] : [
             <img src={profile_image} alt="" className="profile-img-navbar" />
           ]}
-          <strong>{comment.account.first_name} <span className="text-muted ms-2">{moment(comment.created_at).fromNow()} </span></strong>
-          {comment.account_id === comment.post.account_id && <a className="text-primary">OC</a>}
+          <strong>{comment.account.first_name} <span className="text-muted ms-2">{moment(comment.created_at).fromNow()}</span></strong>
+
           <div className='ms-4 ml-4'>
             <Markup content={comment.message} />
           </div>
         </div>
-        <div className="fl">
+        <div class="fl">
           <a href="#" onClick={(event) => handleClick(event, comment.id)}>Reply</a>
           {selectedComment === comment.id && <Form parent={comment.id} comment_id={comment.id} />}
           <hr />
