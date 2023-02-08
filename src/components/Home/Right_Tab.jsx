0f7b1f98d41b4ff9a412998ec9af875d6c1dc34f
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-02-08 22:05:56 +0530

    Premium card redesigned

--- src/components/Home/Right_Tab.jsx
+++ src/components/Home/Right_Tab.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
-import {FaShieldAlt} from 'react-icons/fa'
+import {GiCheckedShield} from 'react-icons/gi'
 import '../../css/Tabright.css'
 import reddit_home_avatar from '../../images/reddit_home_avatar.png'
 
@@ -11,17 +11,17 @@ const account_url = "http://localhost:3000"
 const Right_Tab = () => {
   return (
     <div className="">
-      <div className="card p-3">
+      <div className="card p-1">
         <div className="row">
-          <div className="col-2">
-            <i className="premium-logo"><FaShieldAlt /></i>
+          <div className="col-1">
+            <i className="premium-logo"><GiCheckedShield /></i>
           </div>
           <div className="col-10">
-            <p className="premium-head mb-1">Reddit Premium</p>
-            <p className="premium-des">The best Reddit experience, with monthly Coins</p>
+            <p className="premium-heading mb-1">Reddit Premium</p>
+            <p className="premium-description">The best Reddit experience, with monthly Coins</p>
           </div>
         </div>
-        <Button className="w-100 premium-join text-center mb-3 text-white">Try Now</Button>
+        <Button className="w-100 premium-join text-center mb-2 text-white">Try Now</Button>
       </div>
       <div className="card mt-3">
         <img src="https://www.redditstatic.com/desktop2x/img/id-cards/home-banner@2x.png" className="cover-img home-small-cover" alt="cover" />
