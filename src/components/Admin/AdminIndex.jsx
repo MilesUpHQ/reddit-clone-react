63416c5fe07bd5f4f87489a3086dcc9a6c5c040d
mithun30052001 <lmithun01@gmail.com>
2023-02-08 15:43:28 +0530

    PostList is created successfully in admin/post page and data is fetched using DataProvider

--- src/components/Admin/AdminIndex.jsx
+++ src/components/Admin/AdminIndex.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
-import {Admin, Resource} from 'react-admin'
-import restProvider from 'ra-data-simple-rest'
+import { Admin, Resource } from 'react-admin'
+import axios from 'axios'
 import PostList from './PostList'
 
-function AdminIndex() {
+const dataProvider = axios.create({
+  baseURL: 'http://localhost:3001/api/v1/communities/1/',
+});
+
+const AdminIndex = () => {
   return (
-    <Admin dataProvider={restProvider(`http://localhost:3000/api/v1/communities/1/posts/`)}>
-        <Resource name='adminposts' list={PostList} />
+    <Admin dataProvider={async (type, resource) => {
+      try {
+        const requestConfig = {
+          method: 'GET',
+          url: resource,
+          headers: { 'Content-Type': 'application/json' },
+        };
+        const response = await dataProvider(requestConfig);
+        let data = response.data.posts;
+        if (!Array.isArray(data)) {
+          data = [data];
+        }
+        const newData = data.map((item, index) => ({ id: index, ...item }));
+        console.log(newData);
+        return { data: newData, total: newData.length };
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }}>
+      <Resource name='posts' list={PostList} />
     </Admin>
   )
-}
+};
 
-export default AdminIndex
+export default AdminIndex;
