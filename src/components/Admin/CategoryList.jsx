0c45fb3b7a9576a49ec070edcfde3fbd0e3af587
mithun30052001 <lmithun01@gmail.com>
2023-02-09 22:43:07 +0530

    Extra fields added to CategoryList

--- src/components/Admin/CategoryList.jsx
+++ src/components/Admin/CategoryList.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { List, Datagrid, TextField, Create, SimpleForm, TextInput } from 'react-admin';
+import { List, Datagrid, TextField, Create, SimpleForm, TextInput,DateField } from 'react-admin';
 
 const CategoryList = (props) => (
     <List {...props}>
         <Datagrid rowClick="edit">
             <TextField source="id" />
             <TextField source="name" />
+            <DateField source="created_at" />
+            <DateField source="updated_at" />
         </Datagrid>
     </List>
 );
