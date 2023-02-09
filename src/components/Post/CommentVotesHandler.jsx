fdfa31eedc27713c52bc30c13a293fb7c1d55abc
Aboorva1 <abookannan567@gmail.com>
2023-02-09 17:05:55 +0530

    use effect added

--- src/components/Post/CommentVotesHandler.jsx
+++ src/components/Post/CommentVotesHandler.jsx
@@ -1,22 +1,31 @@
 import { React, useState, useEffect } from 'react'
 import { TbArrowBigTop, TbArrowBigDown } from 'react-icons/tb'
 import { GoArrowUp, GoArrowDown } from 'react-icons/go'
+import { useParams } from 'react-router-dom';
 import axios from 'axios'
 
-const CommentVotesHandler = ({ comment, communityId, commentId, voteCount }) => {
+const CommentVotesHandler = ({ comment, commentId, voteCount }) => {
   const account = JSON.parse(localStorage.getItem('account'))
   const [count, setCount] = useState();
+  let { id, community_id } = useParams();
   const [upvoteClass, setupvoteClass] = useState("");
   const [downvoteClass, setdownvoteClass] = useState("");
   const commentVote = account && comment.votes ? comment.votes.find(comment_vote => comment_vote.account_id === account.id) : null;
 
+  useEffect(() => {
+    setCount(voteCount);
+    setupvoteClass(commentVote && commentVote.value === 1 ? 'voted' : '');
+    setdownvoteClass(commentVote && commentVote.value === -1 ? 'voted' : '');
+  }, [comment]);
+
+  
   return (
-    <div  style={{display: "flex"}} className=''>
+    <div style={{ display: "flex" }} className='mt-0'>
       <div className={`vote-icon upvote ${upvoteClass}`}>
         {upvoteClass ?
-          <GoArrowUp  />
+          <GoArrowUp />
           :
-          <TbArrowBigTop  />
+          <TbArrowBigTop />
         }
       </div>
       <span className={`vote-score upvote_${upvoteClass} downvote_${downvoteClass}`}>{count}</span>
