b28e02f60aa055c43194a64c7bc5398b88083c06
Vasanthakumar <vasanthakumara117@gmail.com>
2023-02-09 13:01:39 +0530

    Added AboutCommunity and RulesCommunity Component

--- src/components/Post/CommunityDetails.jsx
+++ src/components/Post/CommunityDetails.jsx
@@ -1,9 +1,15 @@
+import moment from 'moment';
 import React, { useEffect, useState } from 'react'
+import { BiDotsHorizontalRounded } from 'react-icons/bi';
+import { GiCakeSlice } from 'react-icons/gi';
+import { GoPrimitiveDot } from 'react-icons/go';
 import { useParams, Link } from 'react-router-dom';
 import cover_image from '../../images/Cover-Image.jpg';
 import reddit_logo from '../../images/reddit-logo.png'
+import AboutCommunity from '../Community/AboutCommunity';
+import RulesCommunity from '../Community/RulesCommunity';
 
-const CommunityDetails = ({ post }) => {
+const CommunityDetails = ({ post, isBanned }) => {
 
   let { community_id } = useParams();
 
@@ -15,33 +21,8 @@ const CommunityDetails = ({ post }) => {
 
   return (
     <div>
-      <div className="card" key={post.id}>
-        <div className="card-body">
-          <div className="text-center">
-            <Link to={`/r/${community_id}`}>
-              {post.community && post.community.cover_image && post.community.cover_image.url ? [
-                <img src={`http://localhost:3000${post.community.cover_image.url}`} alt="" className="card-img-top home-small-cover" />
-              ] : [
-                <img src={cover_image} alt="" className="card-img-top home-small-cover" />
-              ]}
-              {post.community && post.community.profile_image && post.community.profile_image.url ? [
-                <img src={`http://localhost:3000${post.community.profile_image.url}`} alt="" className="ml-3 home-small-profile" />
-              ] : [
-                <img src={reddit_logo} alt="" className="ml-3 home-small-profile" />
-              ]}
-            </Link>
-            <h5 className="card-title"><Link to={`/r/${community_id}`}>{post.community && post.community.name}</Link></h5>
-          </div>
-        </div>
-      </div>
-      <div className="card mt-5">
-        <h5 className="rules-card-title">Community Rules</h5>
-        <div className="card-body">
-          <p className="card-text">
-            {post.community && post.community.rules ? post.community.rules : ''}
-          </p>
-        </div>
-      </div>
+      <AboutCommunity community={post.community} isBanned={isBanned} postShow={true} />
+      <RulesCommunity community={post.community} />
     </div>
   )
 }
