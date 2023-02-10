3ec7b1dc3f1a9f42235f0ea8e3efa00e643fe7c8
tushar R <tusharrravindran@gmail.com>
2023-02-10 16:04:35 +0530

    PROFILE PAGE POST FIXED

--- src/components/Accounts/posts.jsx
+++ src/components/Accounts/posts.jsx
@@ -6,31 +6,42 @@ import PostList from '../Post/PostList';
 const Posts = () => {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
-  const limit = 15;
+  const [isLoading, setIsLoading] = useState(false);
+  const limit = 1;
   const account = JSON.parse(localStorage.getItem('account'));
 
   useEffect(() => {
-    axios.get(`http://localhost:3000/api/v1/communities/1/posts?page=${page}&limit=${limit}`)
+    loadPosts();
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop
+      === document.documentElement.offsetHeight
+    ) {
+      loadPosts();
+    }
+  };
+
+
+
+  const loadPosts = () => {
+    if (isLoading) return;
+    setIsLoading(true);
+    axios
+      .get(`http://localhost:3000/api/v1/communities/1/posts?page=${page}&limit=${limit}`)
       .then(response => {
-        setPosts([...posts, ...response.data.posts]);
-        setHasMore(response.data.total_posts > page);
+        setPosts(posts.concat(response.data.posts));
+        setPage(page + 1);
+        setIsLoading(false);
       })
       .catch(error => {
         console.error(error);
+        setIsLoading(false);
       });
-
-    window.onscroll = () => {
-      if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight && hasMore) {
-        setPage(prevPage => prevPage + 1);
-      }
-    };
-
-    return () => {
-      window.onscroll = null;
-    };
-  }, [hasMore, page, limit]);
-
+  };
 
   return (
     <div>
