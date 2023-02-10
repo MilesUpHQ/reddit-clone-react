ebecd6c2fd793eea5b3ed357fd57c0a171719ebc
Aboorva1 <abookannan567@gmail.com>
2023-02-10 15:20:25 +0530

    Spacing adjusted

--- src/components/Post/CommentVotesHandler.jsx
+++ src/components/Post/CommentVotesHandler.jsx
@@ -27,52 +27,51 @@ const CommentVotesHandler = ({ comment, commentId, voteCount }) => {
     }
   }, [account, community_id, id, commentId]);
 
-
   const handleVote = async (value) => {
     if (!account) {
       return;
     }
     const response = await axios.get(url);
     let comment_vote = response.data.find(comment_vote => comment_vote.comment_id === commentId && comment_vote.account_id === account.id);
-    
+
     if (comment_vote) {
-        if (comment_vote.value === value) {
-            await axios.delete(`${url}/${comment_vote.id}`);
-            setCount(count - value);
-            if (value === 1) {
-                setUpvoteClass("");
-            } else {
-                setDownvoteClass("");
-            }
-        } else {
-            await axios.patch(`${url}/${comment_vote.id}`, {
-                comment_vote: { value }
-            });
-            setCount(count + 2 * value);
-            if (value === 1) {
-                setUpvoteClass("voted");
-                setDownvoteClass("");
-            } else {
-                setDownvoteClass("voted");
-                setUpvoteClass("");
-            }
-        }
-    } else {
-        await axios.post(url, {
-            comment_vote: {
-                value,
-                account_id: account.id
-            }
-        });
-        setCount(count + value);
+      if (comment_vote.value === value) {
+        await axios.delete(`${url}/${comment_vote.id}`);
+        setCount(count - value);
         if (value === 1) {
-            setUpvoteClass("voted");
+          setUpvoteClass("");
         } else {
-            setDownvoteClass("voted");
+          setDownvoteClass("");
         }
+      } else {
+        await axios.patch(`${url}/${comment_vote.id}`, {
+          comment_vote: { value }
+        });
+        setCount(count + 2 * value);
+        if (value === 1) {
+          setUpvoteClass("voted");
+          setDownvoteClass("");
+        } else {
+          setDownvoteClass("voted");
+          setUpvoteClass("");
+        }
+      }
+    } else {
+      await axios.post(url, {
+        comment_vote: {
+          value,
+          account_id: account.id
+        }
+      });
+      setCount(count + value);
+      if (value === 1) {
+        setUpvoteClass("voted");
+      } else {
+        setDownvoteClass("voted");
+      }
     }
   };
-        
+
   const handleUpvote = () => handleVote(1);
   const handleDownvote = () => handleVote(-1);
 
