3b0fbb8fb021ec7ae6f1679a88f86ccabaa91808
mithun30052001 <lmithun01@gmail.com>
2023-02-12 20:07:32 +0530

    Category name of respective row is passed as params to create community page when redirected via custom links

--- src/components/Admin/CategoryList.jsx
+++ src/components/Admin/CategoryList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { List, Datagrid, TextField, Create, SimpleForm, TextInput, DateField, EditButton, DeleteButton, Edit, required, useRecordContext } from 'react-admin';
+import { List, Datagrid, TextField, Create, SimpleForm, TextInput, DateField, EditButton, DeleteButton, Edit, required, useRecordContext, Link } from 'react-admin';
 
 const handleDelete = async (id) => {
     try {
@@ -10,18 +10,28 @@ const handleDelete = async (id) => {
     }
 };
 
-const CategoryList = (props) => (
+const CustomLinkField = ({target, to }) => {
+    const category = useRecordContext();
+    return <a href={`${to}?name=${category.name}`} target={target}>Create Community</a>;
+};
+
+const CategoryList = (props) => {
+    return(
+    <>
     <List {...props}>
         <Datagrid rowClick="edit">
             <TextField source="id" />
             <TextField source="name" />
             <DateField source="created_at" />
             <DateField source="updated_at" />
+            <CustomLinkField target="_blank" to={`http://localhost:3001/communities/create`} />
             <EditButton basePath='/categories' />
             <DeleteButton basePath='/categories' onClick={(id) => {handleDelete(id)}} />
         </Datagrid>
     </List>
-);
+    </>
+    )
+};
 
 export const CategoryCreate = (props) => (
     <Create {...props}>
@@ -31,12 +41,15 @@ export const CategoryCreate = (props) => (
     </Create>
 );
 
-const CategoryName = () => {
-  const record = useRecordContext();
-  return <span>Edit {record ? `"${record.name}"` : ''} </span>;
-};
 
 const validateNull = (sourceName) => [required(`${sourceName} is required`)];
+
+
+const CategoryName = () => {
+    const record = useRecordContext();
+    return <span>Edit {record ? `"${record.name}"` : ''} </span>;
+ };
+  
 
  export const CategoryEdit = () => (
     <Edit title={<CategoryName />}>
