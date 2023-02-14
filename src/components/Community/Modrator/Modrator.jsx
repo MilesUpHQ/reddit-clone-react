ed4e7602251bc1eaec525bdec04913e3e2a0adf7
Aboorva1 <abookannan567@gmail.com>
2023-02-14 16:29:48 +0530

    Added tab content for mod-queue

--- src/components/Community/Modrator/Modrator.jsx
+++ src/components/Community/Modrator/Modrator.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import BannedUserList from './BannedUserList'
+import ToolPage from './ToolPage'
 import BannedUsers from './BannedUsers'
 import '../../../css/Community.css'
 
@@ -14,6 +15,7 @@ import { BiBarChartAlt } from 'react-icons/bi';
 import { AiOutlineQuestionCircle } from 'react-icons/ai';
 import { RiFileListLine } from 'react-icons/ri';
 import { RiFileShieldLine } from 'react-icons/ri';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
 // import { HiOutlineArrowTopRightOnSquare } from 'react-icons/hi';
 
 
@@ -21,63 +23,72 @@ const Modrator = () => {
   const { id } = useParams();
 
   return (
-    <div>
+    <div className=''>
       <div className=''></div>
-      <nav id="sidebar" className="bg-light sidebar side-nav mt-4 ms-2">
-        <div className="sidebar-sticky">
-          <ul className="nav flex-column">
-            <div className=" ms-3 text-muted mb-1"><span className='tools'><RiFileShieldLine /></span><span className='mod-nav'>QUEUES</span></div>
-            <li className="nav-item"><a href="#mod-queue">Mod queue</a></li>
-            <a href="#"><li className="nav-item">Reports</li></a>
-            <a href="#"><li className="nav-item">Spam</li></a>
-            <a href="#"><li className="nav-item">Edited</li></a>
-            <a href="#"><li className="nav-item">Unmoderated</li></a>
+      <div className='sidebar'>
+        <nav className="bg-light side-nav">
+          <div className="sidebar-sticky">
+            <ul className="nav flex-column">
+              <div className=" ms-3 text-muted mb-1"><span className='tools'><RiFileShieldLine /></span><span className='mod-nav'>QUEUES</span></div>
+              <li className="nav-item"><a href="#mod-queue">Mod queue</a></li>
+              <a href="#"><li className="nav-item">Reports</li></a>
+              <a href="#"><li className="nav-item">Spam</li></a>
+              <a href="#"><li className="nav-item">Edited</li></a>
+              <a href="#"><li className="nav-item">Unmoderated</li></a>
 
-            <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><FaRegUser /></span><span className='mod-nav'> USER MANAGEMENT</span></div>
-            <a href="#"><li className="nav-item">Banned</li></a>
-            <a href="#"><li className="nav-item">Muted</li></a>
-            <a href="#"><li className="nav-item">Approved</li></a>
-            <a href="#"><li className="nav-item">Talk hosts</li></a>
-            <a href="#"><li className="nav-item">Moderators</li></a>
+              <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><FaRegUser /></span><span className='mod-nav'> USER MANAGEMENT</span></div>
+              <a href="#"><li className="nav-item">Banned</li></a>
+              <a href="#"><li className="nav-item">Muted</li></a>
+              <a href="#"><li className="nav-item">Approved</li></a>
+              <a href="#"><li className="nav-item">Talk hosts</li></a>
+              <a href="#"><li className="nav-item">Moderators</li></a>
 
-            <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools mt-2'><AiOutlineTag /></span><span className='mod-nav'> FLAIR & EMOJIS</span></div>
-            <a href="#"><li className="nav-item">Grant user flair</li></a>
-            <a href="#"><li className="nav-item">Emojis</li></a>
-            <a href="#"><li className="nav-item">User flair</li></a>
-            <a href="#"><li className="nav-item">Post flair</li></a>
+              <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools mt-2'><AiOutlineTag /></span><span className='mod-nav'> FLAIR & EMOJIS</span></div>
+              <a href="#"><li className="nav-item">Grant user flair</li></a>
+              <a href="#"><li className="nav-item">Emojis</li></a>
+              <a href="#"><li className="nav-item">User flair</li></a>
+              <a href="#"><li className="nav-item">Post flair</li></a>
 
-            <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><RiFileListLine /></span><span className='mod-nav'> RULES AND REGULATIONS</span></div>
-            <a href="#"><li className="nav-item">Rules</li></a>
-            <a href="#"><li className="nav-item">Removal reasons</li></a>
-            <a href="#"><li className="nav-item">Content controls</li></a>
-            <a href="#"><li className="nav-item">Automod</li></a>
+              <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><RiFileListLine /></span><span className='mod-nav'> RULES AND REGULATIONS</span></div>
+              <a href="#"><li className="nav-item">Rules</li></a>
+              <a href="#"><li className="nav-item">Removal reasons</li></a>
+              <a href="#"><li className="nav-item">Content controls</li></a>
+              <a href="#"><li className="nav-item">Automod</li></a>
 
-            <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><MdOutlineFormatListBulleted /></span><span className='mod-nav'> CONTENT</span></div>
-            <a href="#"><li className="nav-item">Scheduled posts</li></a>
+              <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><MdOutlineFormatListBulleted /></span><span className='mod-nav'> CONTENT</span></div>
+              <a href="#"><li className="nav-item">Scheduled posts</li></a>
 
-            <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><CiSettings /></span><span className='mod-nav'> OTHER</span></div>
-            <a href="#"><li className="nav-item">Awards</li></a>
-            <a href="#"><li className="nav-item">Wiki pages</li></a>
-            <a href="#"><li className="nav-item">Community settings</li></a>
-            <a href="#"><li className="nav-item">Community appearance</li></a>
+              <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><CiSettings /></span><span className='mod-nav'> OTHER</span></div>
+              <a href="#"><li className="nav-item">Awards</li></a>
+              <a href="#"><li className="nav-item">Wiki pages</li></a>
+              <a href="#"><li className="nav-item">Community settings</li></a>
+              <a href="#"><li className="nav-item">Community appearance</li></a>
 
-            <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><MdOutlineMailOutline /></span><span className='mod-nav'> MOD MAIL</span></div>
-            <a href="#"><li className="nav-item">Modmail</li></a>
+              <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><MdOutlineMailOutline /></span><span className='mod-nav'> MOD MAIL</span></div>
+              <a href="#"><li className="nav-item">Modmail</li></a>
 
-            <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><BiBarChartAlt /></span><span className='mod-nav'> COMMUNITY ACTIVITY</span></div>
-            <a href="#"><li className="nav-item">Traffic stats</li></a>
-            <a href="#"><li className="nav-item">Mod log</li></a>
+              <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><BiBarChartAlt /></span><span className='mod-nav'> COMMUNITY ACTIVITY</span></div>
+              <a href="#"><li className="nav-item">Traffic stats</li></a>
+              <a href="#"><li className="nav-item">Mod log</li></a>
 
-            <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><AiOutlineQuestionCircle /></span><span className='mod-nav'> MOD HELP CENTER</span></div>
-            <a href="#"><li className="nav-item">Mod help center</li></a>
-            <a href="#"><li className="nav-item">Moderator code of conduct</li></a>
-            <a href="#"><li className="nav-item">r/ModSupport</li></a>
-            <a href="#"><li className="nav-item">r/ModHelp</li></a>
-            <a href="#"><li className="nav-item">Contact Reddit</li></a>
-          </ul>
+              <div className=" ms-3 text-muted mb-1 mt-3"><span className='tools'><AiOutlineQuestionCircle /></span><span className='mod-nav'> MOD HELP CENTER</span></div>
+              <a href="#"><li className="nav-item">Mod help center</li></a>
+              <a href="#"><li className="nav-item">Moderator code of conduct</li></a>
+              <a href="#"><li className="nav-item">r/ModSupport</li></a>
+              <a href="#"><li className="nav-item">r/ModHelp</li></a>
+              <a href="#"><li className="nav-item">Contact Reddit</li></a>
+            </ul>
+          </div>
+        </nav>
+        <div className='mt-3 mb-5'>
+          <div class="tab-content">
+            <div id="mod-queue" class="tab-pane">
+            <h5>Mod Queue <AiOutlineInfoCircle /></h5>
+              <ToolPage />
+            </div>
+          </div>
         </div>
-      </nav>
-
+      </div>
       <Tab.Container id="left-tabs-example" defaultActiveKey="first">
         <Row>
           {/* <Col sm={3}>
@@ -107,7 +118,7 @@ const Modrator = () => {
                     <BannedUserList />
                   </Col>
                   <Col sm={3}>
-                    <BannedUsers />
+                    {/* <BannedUsers /> */}
                   </Col>
                 </Row>
               </Tab.Pane>
